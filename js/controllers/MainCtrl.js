c707b93faeac326f5b2ea1073822ceb19073ac3c
ready4 <diby.yavo@gmail.com>
2016-04-11 17:13:19 +0000

    first commit

---
+++ js/controllers/MainCtrl.js
@@ -0,0 +1,31 @@
+app.controller('MainCtrl', function($scope, $http) {
+    /*$http.get('appStore.json').success(function (data) {
+                                       $scope.apps = data;
+                                       });*/
+  $scope.apps = [ 
+	  { 
+	    icon: 'img/move.jpg', 
+	    title: 'MOVE', 
+	    developer: 'MOVE, Inc.', 
+	    price: 0.99 
+	  }, 
+	  { 
+	    icon: 'img/shutterbugg.jpg', 
+	    title: 'Shutterbugg', 
+	    developer: 'Chico Dusty', 
+	    price: 2.99 
+	  },
+	  {
+	    icon: 'img/gameboard.jpg',
+	    title: 'Gameboard',
+	    developer: 'Armando P.',
+	    price: 1.99
+	  },
+	  {
+	    icon: 'img/forecast.jpg',
+	    title: 'Forecast',
+	    developer: 'Forecast',
+	    price: 1.99
+	  }
+	];
+});
