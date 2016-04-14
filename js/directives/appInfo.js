012b54d763a2bfc11b9af02e9146d6e71c31e66f
ready4 <diby.yavo@gmail.com>
2016-04-14 14:36:48 +0000

    update index

--- js/directives/appInfo.js
+++ js/directives/appInfo.js
@@ -1,9 +1,9 @@
-app.directive('appInfo', function() {
+app.directive('appInfo', function(){
     return {
         restrict: 'E',
         scope: {
             info: '='
         },
         templateUrl: 'js/directives/appInfo.html'
-    }
-})
+    };
+});
